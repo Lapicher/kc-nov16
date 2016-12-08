346932bd4c8d2e4157ccf85902a35c4a579f1ef3
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-12-07 22:31:51 -0600

    se agrega la clase Resolve para obtener los datos de contacto al cargar la ruta, se quita el servicio por esta ya que no es deber del componente pedir los datos por medio del servicio sino que al cargar la direccion, el routing se encargue de entregarle los datos

--- app/mis-contactos/mis-contactos.component.ts
+++ app/mis-contactos/mis-contactos.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import { Contacto } from "../entidades/contacto";
-import { ContactosService } from "../servicios/contactos.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     templateUrl: "./app/mis-contactos/mis-contactos.component.html"  
@@ -12,13 +12,15 @@ export class MisContactosComponent implements OnInit {
     // Usando el modificador de acceso en un parámetro del constructor, hacemos
     // que TypeScript lo añada como atributo a la instancia que se crea. Así es
     // como hacemos la inyección de dependencias en Angular.
-    constructor(private _contactosService: ContactosService) { }
+    //constructor(private _contactosService: ContactosService) { }
+    constructor(private _activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
 
         // Obtenemos los contactos apoyándonos en el servicio.
-        this._contactosService.obtenerContactos()
-                              .subscribe(contactos => this.listaContactos = contactos);
+        this._activatedRoute.data.forEach((data: { contactos: Contacto[]})=>{
+            this.listaContactos = data.contactos;
+        });
     }
 
     mostrarDetalles(contacto: Contacto):void {
---
+++ app/servicios/contactos-resolve.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
+import { Contacto } from "../entidades/contacto";
+import { Observable } from "rxjs/Observable";
+import { ContactosService } from "./contactos.service";
+
+@Injectable()
+export class ContactosResolve implements Resolve<Contacto[]>{
+
+
+    constructor(private _contactosService: ContactosService){
+
+    }
+
+    resolve(route: ActivatedRouteSnapshot): Observable<Contacto[]>{
+        return this._contactosService.obtenerContactos();
+    }
+
+}
